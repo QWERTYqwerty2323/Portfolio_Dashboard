372926c24927d947d611b4528342cef364edda50
QWERTYqwerty2323 <harmanghattu0069@gmail.com>
2026-09-12 16:11:39 +0530

    Created using Colab

---
+++ NewUntiledreq.py
@@ -0,0 +1,54 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPYoiX9/+8kg4tvlyFtqmaB",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/QWERTYqwerty2323/Portfolio_Dashboard/blob/Main/NewUntiledreq.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "E0sItntNInwR"
+      },
+      "outputs": [],
+      "source": [
+        "streamlit>=1.38\n",
+        "yfinance>=0.2.40\n",
+        "pandas>=2.0\n",
+        "numpy>=1.24\n",
+        "plotly>=5.20"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "id": "-nfZ-EojNDLE"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
